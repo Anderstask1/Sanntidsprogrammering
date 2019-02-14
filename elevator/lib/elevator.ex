2f0c6c4b12fac0b8438dd39a3a28bebb49b020b5
JuanmaPT <juanmp@stud.ntnu.no>
2019-02-14 16:22:31 +0100

    Added elevator driver and example server process

--- elevator/lib/elevator.ex
+++ elevator/lib/elevator.ex
@@ -12,7 +12,21 @@ defmodule Elevator do
       :world
 
   """
+  IO.puts "This only runs doring compilation...."
   def hello do
+    IO.puts "Hello brothres"
     :world
   end
+
+def server(name) do
+  #To run this function:
+  #   iex -S mix
+  #   iex(2)> server_1=spawn(fn -> Elevator.server("server_1") end)
+  #   send(server_1, "here the message") // This is the testing
+    receive do #Wait here until I receive something :D
+      message -> IO.puts "Server #{name} received #{message}"
+    end
+    Elevator.server(name)
+  end
+
 end
