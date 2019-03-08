217ea0d437989fd0cf127786cd2cba335353402e
ludvigvl <ludvigvl@stud.ntnu.no>
2019-03-08 13:18:36 +0100

    created new mix rep for project

---
+++ elevator_project/lib/elevator_project.ex
@@ -0,0 +1,18 @@
+defmodule ElevatorProject do
+  @moduledoc """
+  Documentation for ElevatorProject.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElevatorProject.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
