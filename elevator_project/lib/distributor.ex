f5d57fad023600ef7b0ca594d39400de54a832bf
ludvigvl <ludvigvl@stud.ntnu.no>
2019-03-08 15:38:38 +0100

    added documentatino to all function and all modules

--- elevator_project/lib/distributor.ex
+++ elevator_project/lib/distributor.ex
@@ -1,5 +1,11 @@
 defmodule Distributor do
 
+  @moduledoc """
+  This is the distribitur module. The distributor is a part of the master, doing the computation
+  of shortest-path and cost function, in order to distribute elevator orders. The distribitur in all
+  nodes recieves the list of orders and states, but only the master distribitur distribute. 
+  """
+
   @doc """
   this function says hello
   """
--- elevator_project/lib/elevator.ex
+++ elevator_project/lib/elevator.ex
@@ -1,27 +1,12 @@
 defmodule Elevator do
-  #use GenServer
   #JMPT: I suggest using GenServer(Elixir Generic server) for the state machine
 
-
   @moduledoc """
-  This is the beginning of our project.
-  The actual code only moves the elevator up  and down.
-  JMPT
+  This is the elevator module. The elevator module controls one single elevator,
+  handles internal orders and sends orders and states to distributor. The elevator
+  acts like a slave and complete orders from master.
   """
 
-  @doc """
-  Documentation of the elvator project
-
-  ## Examples
-
-      iex> Elevator.simple_ele()
-      :world
-
-  """
-#   use Application
-#
-#   def hello() do
-#     IO.puts "Hello"
   IO.puts "This only runs during compilation..."
 
   # #GenServer callbacks
@@ -52,55 +37,62 @@ defmodule Elevator do
   #
   # end
 
-
-
+  @doc """
+  this function says hello
+  """
   def hello do
     IO.puts "Hello brothers"
     Distributor.hello
     :world
   end
 
-def simple_ele() do
-  {:ok, driver_pid}=Driver.start() #setup the elevator driver
-  if Driver.set_floor_indicator(driver_pid, 2) == :between_floors do
-    IO.puts "The cab is between floors"
+  @doc """
+  this function controls a single elevator to go up to 3 floor and the down to 0
+  """
+  def simple_ele() do
+    {:ok, driver_pid}=Driver.start() #setup the elevator driver
+    if Driver.set_floor_indicator(driver_pid, 2) == :between_floors do
+      IO.puts "The cab is between floors"
+    end
+    Driver.set_motor_direction(driver_pid, :down)# Elevator go down!
+
+    until_reach_floor(driver_pid, 0)
+    IO.puts "Elevator is in floor 0"
+    Driver.set_motor_direction(driver_pid, :up)# Elevator go up!
+
+    until_reach_floor(driver_pid, 3)
+    IO.puts "Elevator is in floor 3"
+    Driver.set_motor_direction(driver_pid, :down)# Elevator go down!
+
+    until_reach_floor(driver_pid, 0)
+    IO.puts "Elevator is in floor 0"
+
+    Driver.stop(driver_pid)# Stops the connection with the Elevator server
+    # Driver.set_motor_direction(driver_pid, :stop) can be used instead for
+    # stopping the elevator whiout disconnecting with the server
+
   end
-  Driver.set_motor_direction(driver_pid, :down)# Elevator go down!
 
-  until_reach_floor(driver_pid, 0)
-  IO.puts "Elevator is in floor 0"
-  Driver.set_motor_direction(driver_pid, :up)# Elevator go up!
-
-  until_reach_floor(driver_pid, 3)
-  IO.puts "Elevator is in floor 3"
-  Driver.set_motor_direction(driver_pid, :down)# Elevator go down!
-
-  until_reach_floor(driver_pid, 0)
-  IO.puts "Elevator is in floor 0"
-
-  Driver.stop(driver_pid)# Stops the connection with the Elevator server
-  # Driver.set_motor_direction(driver_pid, :stop) can be used instead for
-  # stopping the elevator whiout disconnecting with the server
-
-end
-
-
-def until_reach_floor(driver_pid, floor) do
-  #Blocks the code execution until the floor is changed to the value of floor
-  if floor != Driver.get_floor_sensor_state(driver_pid) do
-    until_reach_floor(driver_pid, floor)
-  else
-    :ok
+  @doc """
+  check if floor is reached by elevator
+  """
+  def until_reach_floor(driver_pid, floor) do
+    #Blocks the code execution until the floor is changed to the value of floor
+    if floor != Driver.get_floor_sensor_state(driver_pid) do
+      until_reach_floor(driver_pid, floor)
+    else
+      :ok
+    end
   end
-end
 
-
-
-def server(name) do
-  #To run this function:
-  #   iex -S mix
-  #   iex(2)> server_1=spawn(fn -> Elevator.server("server_1") end)
-  #   send(server_1, "here the message") // This is the testing
+  @doc """
+  simple server
+  """
+  def server(name) do
+    #To run this function:
+    #   iex -S mix
+    #   iex(2)> server_1=spawn(fn -> Elevator.server("server_1") end)
+    #   send(server_1, "here the message") // This is the testing
     receive do #Wait here until I receive something :D
       message -> IO.puts "Server #{name} received #{message}"
     end
