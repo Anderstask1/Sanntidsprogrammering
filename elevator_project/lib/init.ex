0ae194105adea7bea3ec0ef0f081efbd4211d50d
Sara Lund Ludvigsen <sarall@stud.ntnu.no>
2019-03-22 12:24:06 +0100

    observer og init

--- elevator_project/lib/init.ex
+++ elevator_project/lib/init.ex
@@ -1,9 +1,29 @@
-"defmodule Init
+defmodule Init do
 
   @moduledoc """
   This is the init module. The init module is setting up the system, doing the necessary initial
   config. This includes udp-broadcast to set up the cluster, spawning the modules and sending an empty
   list to all elevators.
   """
+  def ip_to_string ip do
+      :inet.ntoa(ip) |> to_string()
+    end
 
+  def get_my_ip do
+    {:ok, socket} = :gen_udp.open(6789, [active: false, broadcast: true])
+    :ok = :gen_udp.send(socket, {255,255,255,255}, 6789, "test packet")
+    ip = case :gen_udp.recv(socket, 100, 1000) do
+      {:ok, {ip, _port, _data}} -> ip
+      {:error, _} -> {:error, :could_not_get_ip}
+    end
+    :gen_udp.close(socket)
+    ip
+  end
+
+  def init(node_name, tick_time \\ 15000) do
+    ip = get_my_ip() |> ip_to_string()
+    full_name = node_name <> "@" <> ip
+    Node.start(String.to_atom(full_name), :longnames, tick_time)
+    Node.set_cookie :hallo
+  end
 end
--- elevator_project/lib/observer.ex
+++ elevator_project/lib/observer.ex
@@ -5,7 +5,6 @@ other nodes as well. When it detects a new node, this node should be added to a
 sorted list of nodes. The list is sorted by IP-adresses. The node on the top of
 the list should send the list to its distributor.
 How does the old distributor know that it is no longer the master?
-
 spm:
 How do I make the beacon send out a signal to "everyone"?
 How do I make the radar listen for "everything"?
@@ -37,7 +36,7 @@ init(port) initialize the transmitter.
 The initialization runs inside the server process right after it boots
 """
   def init(port) do
-    {:ok, beaconSocket} = :gen_udp.open(port, [active: false, broadcast: true])
+    {:ok, beaconSocket} = :gen_udp.open(port, [active: true, broadcast: true])
     beacon(beaconSocket)
   end
 
@@ -48,7 +47,7 @@ Then it recall itself
 """
   def beacon(beaconSocket) do
     :timer.sleep(1000 + :rand.uniform(500))
-    :ok = :gen_udp.send(beaconSocket, {255,255,255,255}, 45679, "test")
+    :ok = :gen_udp.send(beaconSocket, {10,22,77,37}, 45679, "hei")
     beacon(beaconSocket)
   end
 
@@ -65,25 +64,30 @@ radar(radarSocket) listen for messages sent to its socket.
 If it receive a message from a new node, it should add this node to a list.
 """
   def radar(radarSocket) do
-    ip = case :gen_udp.recv(radarSocket, 1000) do
-      {:ok, {ip, _port, data}} -> ip
+    case :gen_udp.recv(radarSocket, 1000) do
+      {:ok, {ip, _port, data}} -> IO.puts data
       {:error, _} -> {:error, :could_not_receive}
     end
-    ip_to_string(ip)
     radar(radarSocket)
   end
 end
 
 
 defmodule NodeCollector do
-@@moduledoc """
+@moduledoc """
 This is a module for creating and testing code that later can be used in the
 Observer module
 """
 
 @doc """
-Works
+spm:
+How do I make the beacon broadcast widely?
+How do I make the radar listen for "everything"?
+What do I do in def start_link?
+How do they cluster?
+How do I make something public?
 """
+
   def get_my_ip do
     {:ok, socket} = :gen_udp.open(6789, [active: false, broadcast: true])
     :ok = :gen_udp.send(socket, {255,255,255,255}, 6789, "test packet")
@@ -109,6 +113,7 @@ I wnat this to work the most! But not sure if it does or how.
     case [Node.self | Node.list] do
       [:'nonode@nohost'] -> {:error, :node_not_running}
       nodes -> nodes
+
     end
   end
 
@@ -120,5 +125,70 @@ The last line in this function does not work.
     full_name = node_name <> "@" <> ip
     Node.start(String.to_atom(full_name), :longnames, tick_time)
   end
+
+  def collect(ip, data) do
+
+  end
+end
+
+defmodule Beacon do
+  use GenServer
+  @beacon_port 45678
+  @radar_port 45679
+
+  def start_link(port \\ 45678) do
+    GenServer.start_link(__MODULE__, port)
+  end
+
+  @doc """
+  init(port) initialize the transmitter.
+  The initialization runs inside the server process right after it boots
+  """
+    def init(port) do
+      {:ok, beaconSocket} = :gen_udp.open(port, [active: true, broadcast: true])
+      beacon(beaconSocket)
+    end
+
+  @doc """
+  beacon(beaconSocket) takes in a socket number.
+  It sleep for a random amount of time, and then beacons out its own information.
+  Then it recall itself
+  """
+    def beacon(beaconSocket) do
+      :timer.sleep(1000 + :rand.uniform(500))
+      :ok = :gen_udp.send(beaconSocket, {10,22,77,37}, 45679, "test")
+      beacon(beaconSocket)
+    end
+end
+
+defmodule Radar do
+  use GenServer
+  @radar_port 45679
+
+  def start_link(port \\ 45679) do
+    GenServer.start_link(__MODULE__, port)
+  end
+
+  @doc """
+  radar() initialize the reciever.
+  """
+  def init(port) do
+    {:ok, radarSocket} = :gen_udp.open(port, [active: true, broadcast: false])
+    radar(radarSocket)
+  end
+
+  @doc """
+  radar(radarSocket) listen for messages sent to its socket.
+  If it receive a message from a new node, it should add this node to a list.
+  """
+  def radar(radarSocket) do
+    case :gen_udp.recv(radarSocket,100,1000) do
+      {:ok, {ip, _port, data}} -> IO.puts data
+      {:error, _} -> {:error, :could_not_receive}
+    end
+    radar(radarSocket)
+  end
+
+
 
 end
